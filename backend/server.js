e2a23e229ef645a47f88dd93a7f0998b428cdaca
davy-mgr <d.mugire@alustudent.com>
2026-03-26 19:50:47 +0200

    server.js again

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://your-frontend-url.vercel.app'],
+  origin: ['http://localhost:3000'],
   credentials: true
 }));
 
