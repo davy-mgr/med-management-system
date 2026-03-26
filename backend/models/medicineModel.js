86bf18102097d44003fb02f0f0cc9dd8613c2e1a
davy-mgr <d.mugire@alustudent.com>
2026-03-26 19:18:37 +0200

    medicine model

--- backend/models/medicineModel.js
+++ backend/models/medicineModel.js
@@ -1,31 +1,18 @@
-// backend/models/medicineModel.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
-const Supplier = require('./supplierModel');
+const Supplier = require('./supplierModel'); // default export
 
 const Medicine = sequelize.define('Medicine', {
-  name: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
-  },
+  name: { type: DataTypes.STRING, allowNull: false },
   description: DataTypes.TEXT,
-  unit: { 
-    type: DataTypes.STRING, 
-    defaultValue: 'tablet' 
-  },
-  quantity: { 
-    type: DataTypes.INTEGER, 
-    defaultValue: 0 
-  },
-  threshold: { 
-    type: DataTypes.INTEGER, 
-    defaultValue: 5 
-  }
-}, { 
-  timestamps: true 
-});
+  unit: { type: DataTypes.STRING, defaultValue: 'tablet' },
+  quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+  threshold: { type: DataTypes.INTEGER, defaultValue: 5 },
+  supplier_id: { type: DataTypes.INTEGER, allowNull: false } // explicitly define FK
+}, { timestamps: true });
 
-// Relation to Supplier
+// Relations
 Medicine.belongsTo(Supplier, { foreignKey: 'supplier_id', as: 'supplier' });
+Supplier.hasMany(Medicine, { foreignKey: 'supplier_id', as: 'medicines' });
 
 module.exports = Medicine;
